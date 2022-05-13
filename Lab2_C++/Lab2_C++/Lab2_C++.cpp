c26f7ce94c9a0d7e5f3cab1756a97949284d0f17
Vladimirk229 <90317545+Vladimirk229@users.noreply.github.com>
2022-05-13 22:39:59 +0300

    Add files via upload

---
+++ Lab2_C++/Lab2_C++/Lab2_C++.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Progression
+{
+private:
+    int a;
+    int q;
+    int n;
+    int LastNum()
+    {
+
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int m;
+    cout << "Количество прогрессий: ";
+    cin >> m;
+}
